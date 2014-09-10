f2781ee25fe542a98ac24e1d4f04558894a2861c
wmak <william.mak@hotmail.ca>
2014-09-10 10:16:59 -0400

    Decoding finished

--- Python/talon/talon.py
+++ Python/talon/talon.py
@@ -7,7 +7,7 @@ def _encode(loc, scale):
         val = 1000000000
     else:
         val = 2000000000
-    val += loc * scale
+    val += abs(loc) * scale
     if val > 3000000000:
         raise OverflowError
     val = hex(int(val))[2:]
@@ -15,12 +15,25 @@ def _encode(loc, scale):
     second = unichr(int(val[4:], 16))
     return first, second
 
-def encode(lon, lat):
+def encode(lat, lon):
     ''' encode takes a longitude and a latitude and using the algorithm
     described in the README(Todo) creates a 4 character unicode string '''
     first, second = _encode(lat, 10000000)
     third, fourth = _encode(lon, 1000000)
     return first, second, third, fourth
 
+def _decode(first, second, scale):
+    first = "%x%x" % (ord(first), ord(second))
+    val = int(first, 16)
+    loc = 1
+    if val < 2000000000: 
+        loc *= -1
+    val = int(str(val)[1:])
+    loc = val / scale * loc
+    return loc
+
 def decode(code):
-    pass
+    first, second, third, fourth = code
+    lat = _decode(first, second, 10000000.0)
+    lon = _decode(third, fourth, 1000000.0)
+    return lat, lon
