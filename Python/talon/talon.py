aa7dbcc8f808d04ea0880280c67d492bdc4f4d27
wmak <william.mak@hotmail.ca>
2014-10-02 00:57:38 -0400

    Adding some comments

--- Python/talon/talon.py
+++ Python/talon/talon.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 def _encode(loc):
+    # Here we use the first decimal as a flag, if it's negative the first
+    # decimal is 0 or 1. If it's positive we set it to 2 or 3. As well if the
+    # magnitude of the value is over 90 we use 1 or 3.
     if loc < 0:
         val = 0
         if loc < -90:
@@ -12,8 +15,12 @@ def _encode(loc):
         if loc > 90:
             val = 3000000000
             loc -= 90
+    # Here we store the value with the remaining digits.
     val += abs(loc) * 10000000
+    # Truncate anything past the decimal, we can't store those ): then convert
+    # to hex
     val = hex(int(val))[2:]
+    # Covert to unicode
     first = unichr(int(val[:4], 16))
     second = unichr(int(val[4:], 16))
     return first, second
@@ -26,9 +33,12 @@ def encode(lat, lon):
     return first, second, third, fourth
 
 def _decode(first, second):
+    # Get the hex representation of the two characters
     first = "%x%x" % (ord(first), ord(second))
+    # Convert the hex back into it's value
     val = int(first, 16)
     loc = 1
+    # Check the flag and change value accordingly
     if val < 2000000000: 
         loc *= -1
     increase = str(val)[1] == 1 or str(val)[1] == 3
