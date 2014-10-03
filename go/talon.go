df91602142fe92e1c08f325383b6710154a260e7
wmak <william.mak@hotmail.ca>
2014-10-02 20:41:36 -0400

    woot woot decode is done

--- go/talon.go
+++ go/talon.go
@@ -31,3 +31,26 @@ func Encode(lat, lon float64) string {
 	}
 	return _encode(lat) + _encode(lon)
 }
+
+func Decode(code string) (lat, lon float64) {
+	var _decode = func(character []rune) float64 {
+		var loc float64
+		first := strconv.QuoteRuneToASCII(character[0])
+		second := strconv.QuoteRuneToASCII(character[1])
+		val, _ := strconv.ParseInt("0x" + first[3:7] + second[3:7], 0, 32)
+		sval := strconv.FormatInt(val, 10)
+		loc = 1
+		if (val < 2000000000){
+			loc = -1
+		}
+		if (val > 1000000000){
+			val, _ = strconv.ParseInt(sval[2:], 0, 0)
+		}
+		fval := math.Mod(float64(val), 1000000000)
+		if (sval[1] == 1 || sval[1] == 3) {
+			fval += 90
+		}
+		return fval/10000000 * loc
+	}
+	return _decode([]rune(code)[:2]), _decode([]rune(code)[2:])
+}
