7e888c608a17af51bd11006894fc60c246948613
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2014-11-06 11:56:44 -0500

    Add comments, and fix style errors from last commit. Version bump.

--- Ruby/lib/talon.rb
+++ Ruby/lib/talon.rb
@@ -3,22 +3,27 @@
 # Encode long co-ordinate pairs into a small utf-8 encoded string.
 module Talon
   def self.encode(lat, long)
+    # encode the latitude & longitude,
+    # split each into 2 hex strings, 4 characters long
+    # turn them into unicode characters, and join as string
     [lat, long]
-    .map { |x| format('%08x', _encode(x)).scan(/.{4}/) }
-    .map { |x| x.map { |y| [y.to_i(16)].pack 'U' } }
-    .join
+      .map { |x| format('%08x', _encode(x)).scan(/.{4}/) }
+      .map { |x| x.map { |y| [y.to_i(16)].pack 'U' } }
+      .join
   end
 
   def self.decode(str)
+    # turn the given string's value into an 8-char hex string
+    # decode the integer representation of that
     str.scan(/.{2}/)
-    .map { |x| format('%04x%04x', x[0].ord, x[1].ord).to_i(16) }
-    .map { |x| _decode(x) }
+       .map { |x| format('%04x%04x', x[0].ord, x[1].ord) }
+       .map { |x| _decode(x.to_i(16)) }
   end
 
   private
 
   def self._encode(loc)
-    location = loc.abs
+    location = loc.abs  # so that the function remains non-destructive
 
     encoding = loc >= 0 ? 2_000_000_000 : 0
 
@@ -31,7 +36,7 @@ module Talon
   end
 
   def self._decode(enc)
-    encoding = enc # so that the function remains non-destructive
+    encoding = enc  # so that the function remains non-destructive
 
     positive = encoding >= 2_000_000_000
     encoding -= 2_000_000_000 if positive
--- Ruby/talon.gemspec
+++ Ruby/talon.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name         = 'talon'
-  s.version      = '0.0.1'
+  s.version      = '0.0.2'
   s.date         = '2014-09-24'
   s.licenses     = ['MIT']
 
